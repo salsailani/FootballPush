d459daa7ccbabfdb999b6dde581d1b10b291a18e
salsailani <sf.sailani@gmail.com>
2020-05-20 03:28:14 -0300

    Fixed the image button and spinner in the Fragment

--- app/src/main/java/com/example/sportsappnav/FirstFragment.java
+++ app/src/main/java/com/example/sportsappnav/FirstFragment.java
@@ -2,33 +2,60 @@ package com.example.sportsappnav;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
 import android.view.ViewGroup;
+import android.widget.Spinner;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ImageButton;
+import android.content.Intent;
+import android.view.View.OnClickListener;
+import android.app.Activity;
+import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
 
 
 public class FirstFragment extends Fragment {
-
+    ImageButton imageButton;
     public FirstFragment() {}
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        // clickable image
-//        ImageView image1 = (ImageView)findViewById(R.id.imageView2);
-//        image1.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v){
-//                startActivity(new Intent(MainActivity.this, addPage.class)); // this line is the problem
-//            }
-//        });
-
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater,
-                             ViewGroup container,
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_first, container, false);
+
+        View view =  inflater.inflate(R.layout.fragment_first, container, false);
+        imageButton = (ImageButton)view.findViewById(R.id.imageButton);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getActivity(), addPage.class));
+            }
+        });
+
+        //fill spinner from string.xml
+        Spinner spinner = (Spinner) view.findViewById(R.id.teams_spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(),
+                R.array.teams_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
+        return view;
     }
 }
