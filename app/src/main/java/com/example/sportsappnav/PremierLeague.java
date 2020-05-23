614b7f01089eeb60859ef64d59b0985cf36dfb9a
salsailani <sf.sailani@gmail.com>
2020-05-23 12:08:23 -0300

    Added the grid layout for the premier league, club icons listener, integer resource file for different screen sizes

--- app/src/main/java/com/example/sportsappnav/PremierLeague.java
+++ app/src/main/java/com/example/sportsappnav/PremierLeague.java
@@ -5,15 +5,56 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
+import android.widget.GridLayout;
+import android.widget.GridView;
+import android.widget.Toast;
 
 
 
 public class PremierLeague extends Fragment {
+
+    GridLayout mainGrid;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_premier_league, container, false);
+        View view = inflater.inflate(R.layout.fragment_premier_league, container, false);
+
+        mainGrid = (GridLayout) view.findViewById(R.id.mainGrid);
+
+        //Set Event
+        setSingleEvent(mainGrid);
+
+
+
+        return view;
     }
 
+    private void setSingleEvent(GridLayout mainGrid) {
+        //Loop all child item of Main Grid
+        for (int i = 0; i < mainGrid.getChildCount(); i++) {
+            //You can see , all child item is CardView , so we just cast object to CardView
+            CardView cardView = (CardView) mainGrid.getChildAt(i);
+            final int finalI = i;
+            cardView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    Intent intent = new Intent(getActivity(), addPage.class);
+                    intent.putExtra("info", "This is activity from card item index  " + finalI);
+                    startActivity(intent);
+
+                }
+            });
+
+        }
+    }
 }
