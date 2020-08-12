11bde3d7ab2399c20b87838dc8c653719dbe6847
Mo Elbadry <mohdelbadry@gmail.com>
2020-08-12 15:15:28 -0400

    faq page

--- app/src/main/java/com/example/footballpush/FAQpage.java
+++ app/src/main/java/com/example/footballpush/FAQpage.java
@@ -1,14 +1,20 @@
 package com.example.footballpush;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import java.util.zip.Inflater;
 
 public class FAQpage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.faq_page);
+
     }
     @Override
     public void onBackPressed() {
